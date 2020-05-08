ab6d2bf7d7088bb90c75870c69f7dfb8b8ad46dd
damstom <tomdams7@gmail.com>
2020-05-08 18:04:07 +0200

    kzen gekapt

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/bierbattleroyale'));
+app.use(express.static(__dirname + '/dist/bier-battle-royale'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/bierbattleroyale/index.html'));
+res.sendFile(path.join(__dirname+'/dist/bier-battle-royale/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
